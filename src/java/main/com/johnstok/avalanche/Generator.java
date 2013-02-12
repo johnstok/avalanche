adbbb4379413deb00d5156f356a2e8c8db70dc69
Keith Webster Johnston <k.w.johnston@gmail.com>
2013-02-12 21:11:15 +0000

    Added the core testing API.

---
+++ src/java/main/com/johnstok/avalanche/Generator.java
@@ -0,0 +1,35 @@
+/*-----------------------------------------------------------------------------
+ * Copyright © 2013 Keith Webster Johnston.
+ * All rights reserved.
+ *
+ * This file is part of avalanche.
+ *
+ * avalanche is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * avalanche is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with avalanche. If not, see <http://www.gnu.org/licenses/>.
+ *---------------------------------------------------------------------------*/
+package com.johnstok.avalanche;
+
+import java.util.concurrent.Future;
+
+
+/**
+ * A generator generates work.
+ *
+ * @param <T>
+ *
+ * @author Keith Webster Johnston.
+ */
+public interface Generator<T> {
+
+    Future<T> generate(Callback callback);
+}
